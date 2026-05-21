6b2edcb44ae928a19e6398bfb5c315b94c980cc2
Raúl Veces <rveces@sanmiguelito.gob.pa>
2026-05-21 07:19:37 -0500

    feat: create users migration

---
+++ backend/migrations/20260521121804_create_users.js
@@ -0,0 +1,25 @@
+exports.up = async function (knex) {
+
+    await knex.schema.createTable(
+        'users',
+        (table) => {
+
+            table.increments('id').primary();
+
+            table.string('email').unique().notNullable();
+
+            table.string('password').notNullable();
+
+            table.timestamps(true, true);
+
+        }
+
+    )
+
+};
+
+exports.down = async function (knex) {
+
+    await knex.schema.dropTableIfExists('users')
+
+};
